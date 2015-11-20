fc06856802c9d42f9682a56d4cea12e01ffa645d
Théophile <Thophile@PC-Thophile>
2015-11-20 17:46:25 +0100

    Removing 'main' from Display.java

--- src/perceptron/Display.java
+++ src/perceptron/Display.java
@@ -18,24 +18,6 @@ public class Display {
 	
 	private static Plot2DPanel plot2D;
 	private static int iLines;
-
-	public static void main(String s[]) throws InterruptedException {
-		
-		// Create a display
-		Display d = new Display();
-		d.show();
-		
-		// Generate curves
-		double [][] l1 = new double[100][2], l2 = new double [100][2];
-		for (int i = 0; i < 100; i++) {
-			l1[i][0] = l2[i][0] = i;
-			l1[i][1] = i * i + 5;
-			l2[i][1] = i * (i / 2)  - 7;
-		}
-		d.addCurve(l1, new Color(0,255,0));
-		d.addCurve(l2, new Color(255,0,0));
-		
-	}
 	
 	public Display () {
 		plot2D = new Plot2DPanel();
