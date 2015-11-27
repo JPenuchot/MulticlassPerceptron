a53cadbc87c615e0a89e101f7948eaaf6267a6fd
Jules Pénuchot <jules.penuchot@u-psud.fr>
2015-11-27 10:27:03 +0100

    Added test stuff and all...

--- src/image/Image.java
+++ src/image/Image.java
@@ -10,15 +10,21 @@ public class Image {
 		String sPath = "./", sImages = sPath + "train-images-idx3-ubyte", sLabels = sPath + "train-labels-idx1-ubyte";
 		MnistReader mrDB = new MnistReader(sLabels, sImages);
 		
-		int iMaxIdx = 10000;
+		int iTrainMaxIdx = 10000;
+		int iTestMaxIdx = 20000;
 		
 		//	Création et init du perceptron multiclasse (10 classes, et on prend le vecteur résultant de la paramétrisation
 		//	de la première image comme référence concernant la taille des paramètres).
 		MultiClass mcPerceptron = new MultiClass(10, ImageUtils.img2BinVect(mrDB.getImage(1)).length);
 		
-		for(int i = 1; i <= iMaxIdx; i++){
+		for(int i = 1; i <= iTrainMaxIdx; i++){
 			mcPerceptron.addTrainData(ImageUtils.img2BinVect(mrDB.getImage(i), 10.));
 			mcPerceptron.addTrainLabel(mrDB.getLabel(i));
+		}
+		
+		for(int i = iTrainMaxIdx; i <= iTestMaxIdx; i++){
+			mcPerceptron.addTestData(ImageUtils.img2BinVect(mrDB.getImage(i), 10.));
+			mcPerceptron.addTestLabel(mrDB.getLabel(i));
 		}
 		
 		//	Moulinette
